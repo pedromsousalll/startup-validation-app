8e129748da2b4c979a577db230432b45bf10feef
pedromsousalll <pedromsousal@icloud.com>
2025-04-25 19:50:46 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost', 'vercel.app', 'example.com'],
+  },
+  experimental: {
+    serverActions: true,
+  },
+}
 
 module.exports = nextConfig
