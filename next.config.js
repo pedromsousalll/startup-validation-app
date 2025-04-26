19a057ad38a9543f0ec6e9c5fe4a34820e0158fa
pedromsousalll <pedromsousal@icloud.com>
2025-04-26 20:40:32 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['localhost', 'vercel.app', 'example.com'],
   },
-  i18n: require('./next-i18next.config').i18n,  // Corrigido
 };
 
 module.exports = nextConfig;
