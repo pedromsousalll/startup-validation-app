fbd3ca95f26c15d16f805dff5711cc9ebd98ada7
Pedro Lopes <pedrolopes@MacBook-Air-de-Pedro.local>
2025-04-26 22:29:11 +0100

    fix: Import PrismaClient in prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,18 @@
-import prisma from "@/lib/prisma";
+// src/lib/prisma.ts
+import { PrismaClient } from '@prisma/client'; // <-- ADICIONE ESTA LINHA
 
 const prismaClientSingleton = () => {
   return new PrismaClient({
-    log: ['query', 'error', 'warn'],
+    log: ['query', 'error', 'warn'], // Configure os logs como desejar
   });
+};
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
-}
-
-export const prisma =
-  globalForPrisma.prisma || prismaClientSingleton();
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+const prisma = globalThis.prisma ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
